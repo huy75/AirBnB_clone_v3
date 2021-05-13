f8e98ebc6553f2ea12e9294f8ab6791dd616f63a
huy75 <huy.nguyen66@gmail.com>
2021-05-13 16:17:12 +0000

    fix semistandard

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,13 @@
 $(document).ready(() => {
-  const checklist = {}
-    $('INPUT:checkbox').change(function() {
-      if ($(this).is(':checked')) {
-        let key = $(this).attr('data-id')
-	let value = $(this).attr('data-name')
-	checklist[key] = value
-      }
-      if (!$(this).is(':checked')) {
-	delete checklist[$(this).attr('data-id')]
-      }
-    })
-})
+  const checklist = {};
+  $('INPUT:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      const key = $(this).attr('data-id');
+      const value = $(this).attr('data-name');
+      checklist[key] = value;
+    }
+    if (!$(this).is(':checked')) {
+      delete checklist[$(this).attr('data-id')];
+    }
+  });
+});
