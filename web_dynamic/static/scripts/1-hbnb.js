6a35234821691514f6cb7c6cfb9f1f9f93469ee6
aureliewouy <aureliecedia@yahoo.com>
2021-05-14 12:27:41 +0000

    add into a list

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   const checklist = {};
-  const list = [];
+  let amen = [];
   $('INPUT:checkbox').change(function () {
     if ($(this).is(':checked')) {
       const key = $(this).attr('data-id');
@@ -12,7 +12,7 @@ $(document).ready(() => {
     if (!$(this).is(':checked')) {
       delete checklist[$(this).attr('data-id')];
     }
-    const amen = Object.values(checklist);
+    amen = Object.values(checklist);
     $('.amenities h4').text(amen.join(', '));
   });
 });
