98ec7d72767f3c570550f54aee84060a1eb244af
huy75 <huy.nguyen66@gmail.com>
2021-05-14 08:01:03 +0000

    feat task 4 introduce ajax

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -13,9 +13,19 @@ $(document).ready(() => {
 
   $.get('http://0.0.0.0:1107/api/v1/status', data => {
     if (data.status === 'OK') {
-      $('DIV#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('DIV#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
+});
+
+$.ajax({
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  contentType: 'application/json',
+  data: ,
+  function () {
+  }
+
 });
