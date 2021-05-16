1c150cbd782c186071ac55fd172797a40949d770
huy75 <huy.nguyen66@gmail.com>
2021-05-16 08:52:32 +0000

    fix task 5 refactor

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+const checkAmenities = function () {
   const checklist = {};
   let amen = [];
   $('INPUT:checkbox').change(function () {
@@ -13,7 +13,9 @@ $(document).ready(() => {
     amen = Object.values(checklist);
     $('.amenities h4').text(amen.join(', '));
   });
+};
 
+const checkApiStatus = function () {
   $.get('http://0.0.0.0:5001/api/v1/status', data => {
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
@@ -21,39 +23,42 @@ $(document).ready(() => {
       $('div#api_status').removeClass('available');
     }
   });
+};
 
-$.ajax({
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  contentType: 'application/json',
-  data: '{}'
-}).done(function (data) {
-  $.each(data, function (index, place) {
-    $.get('http://0.0.0.0:5001/api/v1/users/'+ place.user_id, data => {
-	const first = data.first_name
-	const last = data.last_name
-	const str =
-    `<article>
-	  <div class="title_box">
-          <h2>${place.name}</h2>
-          <div class="price_by_night">$${place.price_by_night}</div>
-	  </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest} Guests</div>
-            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-          </div>
-	  <div class="user">
-            <b>Owner:</b> ${first} ${last}
-          </div>
-          <div class="description">
-            ${place.description}
-          </div>
-        </article>`;
-    $('section.places').append(str);
-  });});
-  }
-);
+const fetchPlaces = function (amenitiesFilter = {}) {
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    data: '{}'
+  }).done(function (data) {
+    $.each(data, function (index, place) {
+      $.get('http://0.0.0.0:5001/api/v1/users/'+ place.user_id, data => {
+    const first = data.first_name
+    const last = data.last_name
+    const str =
+      `<article>
+      <div class="title_box">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">$${place.price_by_night}</div>
+      </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guests</div>
+              <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+            </div>
+      <div class="user">
+              <b>Owner:</b> ${first} ${last}
+            </div>
+            <div class="description">
+              ${place.description}
+            </div>
+          </article>`;
+      $('section.places').append(str);
+    });});
+    }
+  );
+};
 
 $("section button").click(function() {
     $.ajax({
@@ -90,3 +95,12 @@ $("section button").click(function() {
 });
 });
  });
+
+$(() => {
+  checkAmenities();
+  checkApiStatus();
+  fetchPlaces(checkedAmenities);
+  $('button').on('click', () => {
+    fetchPlaces(checkedAmenities);
+  });
+});
