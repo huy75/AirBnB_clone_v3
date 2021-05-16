e1205ee703ceea909a2561e6bd73f7538fdb1222
aureliewouy <aureliecedia@yahoo.com>
2021-05-16 13:49:33 +0000

    remove duplicated

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -31,8 +31,9 @@ const fetchPlaces = function (amenitiesFilter = {}) {
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     contentType: 'application/json',
-    data: '{}'
+    data: JSON.stringify({ amenities: Object.keys(checklist) })
   }).done(function (data) {
+      $('section.places').empty();
     $.each(data, function (index, place) {
       $.get('http://0.0.0.0:5001/api/v1/users/' + place.user_id, data => {
         const first = data.first_name;
@@ -60,42 +61,6 @@ const fetchPlaces = function (amenitiesFilter = {}) {
     });
   });
 };
-
-$('section button').click(function () {
-  $.ajax({
-    type: 'POST',
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
-    contentType: 'application/json',
-    data: JSON.stringify({ amenities: Object.keys(checklist) })
-  }).done(function (data2) {
-    $('section.places').empty();
-    $.each(data2, function (index, place) {
-      $.get('http://0.0.0.0:5001/api/v1/users/' + place.user_id, user => {
-        const first = user.first_name;
-        const last = user.last_name;
-        const str =
-    `<article>
-      <div class="title_box">
-          <h2>${place.name}</h2>
-          <div class="price_by_night">$${place.price_by_night}</div>
-      </div>
-          <div class="information">
-            <div class="max_guest">${place.max_guest} Guests</div>
-            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-          </div>
-      <div class="user">
-            <b>Owner:</b> ${first} ${last}
-          </div>
-          <div class="description">
-            ${place.description}
-          </div>
-        </article>`;
-        $('section.places').append(str);
-      });
-    });
-  });
-});
 
 $(() => {
   checkAmenities();
